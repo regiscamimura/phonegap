b7ec7d517689e92568a63163449141cf7e23bf56
Regis <regiscamimura@gmail.com>
2014-03-27 16:54:13 -0300

    3

--- js/main.js
+++ js/main.js
@@ -7,37 +7,12 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
 	
-	renderHomeView: function() {
-		var html =
-				"<div class='header'><h1>Home</h1></div>" +
-				"<div class='search-view'>" +
-				"<input class='search-key'/>" +
-				"<ul class='employee-list'></ul>" +
-				"</div>"
-		$('body').html(html);
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-    initialize: function() {
+	initialize: function() {
 		
-		var self = this;
-	
-        this.store = new LocalStorageStore(function() {
-			self.renderHomeView();
+		var self = this;		
+		this.store = new LocalStorageStore(function() {
+			$("body").html(new HomeView(self.store).render().el);
 		});
     }
 
