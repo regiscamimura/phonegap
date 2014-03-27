6d01f95ccae6a9f05839990904f0306a943da2d6
Regis <regiscamimura@gmail.com>
2014-03-27 16:15:41 -0300

    8

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,7 +22,12 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
+		
+		var self = this;
+	
+        this.store = new LocalStorageStore(function() {
+			self.showAlert("Storage Intialized", "Info");
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
